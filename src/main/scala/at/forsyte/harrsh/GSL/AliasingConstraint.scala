43df4ce70a1b6a45ef50e3e48355f4a511a468e0
Matthias Hetzenberger <mhetzenberger@gmail.com>
2021-02-11 20:33:15 +0100

    Improve representation of aliasing constraints

--- src/main/scala/at/forsyte/harrsh/GSL/AliasingConstraint.scala
+++ src/main/scala/at/forsyte/harrsh/GSL/AliasingConstraint.scala
@@ -1,23 +1,51 @@
 package at.forsyte.harrsh.GSL
 
-import scala.collection.SortedSet
-import scala.math.Ordering.Implicits.sortedSetOrdering
+import at.forsyte.harrsh.seplog.Var
 
-class AliasingConstraint {
+import scala.collection.SortedSet
+
+case class AliasingConstraint(domain: SortedSet[Var], partition: Map[Var, Var]) {
+  if (domain != partition.keySet)
+    throw new IllegalArgumentException("Inconsistent domains")
+
+  def =:=(t: (Var, Var)): Boolean = domain.contains(t._1) && domain.contains(t._2) && partition(t._1) == partition(t._2)
+
+  def =/=(t: (Var, Var)): Boolean = !(this =:= t)
 }
 
 object AliasingConstraint {
-  def allPartitions[A](set: SortedSet[A])(implicit ordering: Ordering[A]): LazyList[SortedSet[SortedSet[A]]] = {
-    if (set.size <= 1) {
-      LazyList(SortedSet(set))
-    } else {
-      val elem = set.head
-      val partitions = allPartitions(set.tail)
-      partitions.flatMap((partition: SortedSet[SortedSet[A]]) => {
-        LazyList(partition + SortedSet(elem)) ++ partition.foldLeft(LazyList.empty[SortedSet[SortedSet[A]]]) { (seq, set) =>
-          seq.appended((partition - set) + (set + elem))
-        }
-      })
+
+  def allAliasingConstraints(vars: Set[Var]): LazyList[AliasingConstraint] = {
+    allPartitions(vars).map(p => AliasingConstraint(SortedSet.from(vars), p))
+  }
+
+  def mapRepresentationToSet[A](partition: Map[A, A]): Set[Set[A]] = {
+    partition.foldLeft(Set(): Set[Set[A]]) { case (set, (k, v)) =>
+      set.find(_.contains(v)) match {
+        case None => set.incl(Set(k, v))
+        case Some(eq) => set.excl(eq).incl(eq.incl(k))
+      }
     }
+  }
+
+  def allPartitions[A](set: Set[A])(implicit ordering: Ordering[A]): LazyList[Map[A, A]] = {
+
+    def help(seq: Seq[A]): LazyList[Map[A, A]] = {
+      seq match {
+        case Seq() => LazyList(Map.empty)
+        case Seq(e) => LazyList(Map(e -> e))
+        case Seq(head, tail@_*) =>
+          // first is < than all in tail
+          val partitions = help(tail)
+          partitions.flatMap(map => {
+            val first = map.updated(head, head)
+
+            LazyList(first) ++ map.values.toSet.map((e: A) => map.updated(head, e))
+          })
+      }
+    }
+
+    val ordered = set.toSeq.sorted
+    help(ordered)
   }
 }
--- src/test/scala/at/forsyte/harrsh/GSL/AliasingConstraintTest.scala
+++ src/test/scala/at/forsyte/harrsh/GSL/AliasingConstraintTest.scala
@@ -1,22 +1,51 @@
 package at.forsyte.harrsh.GSL
 
+import at.forsyte.harrsh.seplog.FreeVar
 import org.scalatest.flatspec.AnyFlatSpec
 
 import scala.collection.SortedSet
-import scala.math.Ordering.Implicits.sortedSetOrdering
 
 class AliasingConstraintTest extends AnyFlatSpec {
   "AliasingConstraint" should "correctly compute set partitions" in {
-    val part3 = AliasingConstraint.allPartitions(SortedSet(1, 2, 3))
+    val part3 = AliasingConstraint.allPartitions(Set(1, 2, 3))
 
     assert(part3.size == 5)
 
-    assert(part3 == Seq(SortedSet(SortedSet(1), SortedSet(2), SortedSet(3)),
-      SortedSet(SortedSet(1, 2), SortedSet(3)),
-      SortedSet(SortedSet(1, 3), SortedSet(2)),
-      SortedSet(SortedSet(1), SortedSet(2, 3)),
-      SortedSet(SortedSet(1, 2, 3))))
+    assert(part3.toSet.map((s: Map[Int, Int]) => AliasingConstraint.mapRepresentationToSet(s)) == Set(Set(Set(3), Set(2), Set(1)),
+      Set(Set(2), Set(3, 1)),
+      Set(Set(3), Set(2, 1)),
+      Set(Set(3, 2), Set(1)),
+      Set(Set(3, 2, 1))))
 
-    assert(AliasingConstraint.allPartitions(SortedSet.from(1 to 10)).size == 115975)
+    assert(AliasingConstraint.allPartitions(Set.from(1 to 4)).size == 15)
+    assert(AliasingConstraint.allPartitions(Set.from(1 to 10)).size == 115975)
+  }
+
+  it should "correctly determine equalities between variables" in {
+    val (a, b, c) = (FreeVar("a"), FreeVar("b"), FreeVar("c"))
+    val ac = AliasingConstraint(SortedSet(a, b, c), Map((a, a), (b, c), (c, c)))
+
+    assert(a < b && a < c && b < c)
+
+    assert(ac =:= (a, a))
+    assert(ac =/= (a, b))
+    assert(ac =/= (b, a))
+    assert(ac =/= (a, c))
+    assert(ac =/= (c, a))
+    assert(ac =:= (b, b))
+    assert(ac =:= (b, c))
+    assert(ac =:= (c, b))
+    assert(ac =:= (c, c))
+
+    for (e1 <- Seq(a, b, c);
+         e2 <- Seq(a, b, c)) {
+      assert((ac =:= (e1, e2)) == !(ac =/= (e1, e2)))
+    }
+  }
+
+  it should "reject inconsistent domains" in {
+    assertThrows[IllegalArgumentException] {
+      AliasingConstraint(SortedSet.empty, Map((FreeVar("a"), FreeVar("a"))))
+    }
   }
 }
